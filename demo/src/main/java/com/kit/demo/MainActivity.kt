9bdd0cc303b20b9413a72b0ab1e55ac235b9a5fe
kdreamix <wwwyingkit@gmail.com>
2019-04-20 22:11:23 +0800

    Update demo

--- demo/src/main/java/com/kit/demo/MainActivity.kt
+++ demo/src/main/java/com/kit/demo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kit.demo
 
 import android.os.Bundle
 import android.util.Log
+import android.view.animation.DecelerateInterpolator
 import android.view.animation.OvershootInterpolator
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -14,16 +15,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         with(myTab){
-            setTabColor(ContextCompat.getColor(this@MainActivity,R.color.accent_material_dark))
-            setTabBackgroundColor(ContextCompat.getColor(this@MainActivity,R.color.material_blue_grey_900))
-            setLabelTextFont(R.font.montserrat_medium)
-            setLabelTextSize(20f)
-            setLabelTextColor(ContextCompat.getColor(this@MainActivity,R.color.material_grey_600))
-            setTabAnimationInterpolator(OvershootInterpolator())
-            setTabAnimationDuration(300L)
-            setOnTabSelectedListener { position ->
-                Log.v("Main", position.toString())
-            }
+//            setTabColor(ContextCompat.getColor(this@MainActivity,R.color.accent_material_dark))
+//            setTabBackgroundColor(ContextCompat.getColor(this@MainActivity,R.color.material_blue_grey_900))
+//            setLabelTextFont(R.font.montserrat_medium)
+//            setLabelTextSize(20f)
+//            setLabelTextColor(ContextCompat.getColor(this@MainActivity,R.color.material_grey_600))
+            setTabAnimationInterpolator(DecelerateInterpolator())
+//            setTabAnimationDuration(300L)
+//            setOnTabSelectedListener { position ->
+//                Log.v("Main", position.toString())
+//            }
         }
     }
 }
